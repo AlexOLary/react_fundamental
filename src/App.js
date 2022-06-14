17cd2a98dc6eb82e9e5fb5bd138ca1c65bbdb51a
AlexOLary <AlexOLary@gmail.com>
2022-06-14 10:26:53 +0700

    App_css + Counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
+import "./styles/App.css"
 
 function App() {
-    const [likes, setLikes] = useState(0)
     const [value, setValue] = useState("Текст в инпуте")
 
-    function increment(){
-        setLikes(likes + 1)
-    }
-
-    function decrement(){
-        setLikes(likes - 1)
-    }
 
     return(
         <div className="App">
-            <h1>{likes}</h1>
-            <h1>{value}</h1>
-            <input
-                type="text"
-                value={value}
-                onChange={event => setValue(event.target.value)}/>
-          <button onClick={increment}>Increment</button>
-          <button onClick={decrement}>Decrement</button>
+            <div className="post">
+                <div className="post__content">
+                    <strong>1.Javascript</strong>
+                    <div>
+                        Javascript - Язык программирования!
+                    </div>
+                </div>
+                <div className="post__btns">
+                    <button>Удалить</button>
+                </div>
+            </div>
         </div>
     )
 
