91cb0a6fc7e053a093a972e5eb0020d492b948d5
AlexOLary <AlexOLary@gmail.com>
2022-06-19 10:25:13 +0700

    all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import "./styles/App.css"
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
@@ -7,17 +7,44 @@ import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPost] = useState([
-        {id: 1, title: "Javascript", body:"Description"},
-        {id: 2, title: "Javascript 2", body:"Description"},
-        {id: 3, title: "Javascript 3", body:"Description"}
+        {id: 1, title: "Javascript", body: "Description"},
+        {id: 2, title: "Javascript 2", body: "Description"},
+        {id: 3, title: "Javascript 3", body: "Description"}
     ])
 
-    return(
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        setPost([...posts, newPost])
+        setTitle("")
+        setBody("")
+    }
+
+    return (
         <div className="App">
             <form>
-                <MyInput type="text" placeholder="Название поста"></MyInput>
-                <MyInput type="text" placeholder="Описание поста"></MyInput>
-                <MyButton >Создать пост</MyButton>
+                {/*Управляемый компонент*/}
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Название поста"
+                />
+                <MyInput
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
+                    type="text"
+                    placeholder="Описание поста"
+                />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
             </form>
             <PostList posts={posts} title="Посты про JS"/>
         </div>
